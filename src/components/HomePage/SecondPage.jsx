416b577212fbb4c4962004bdb10c6956a78a5e00
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-07-11 12:26:51 +0530

    responsiveness in second page and tabs

--- src/components/HomePage/SecondPage.jsx
+++ src/components/HomePage/SecondPage.jsx
@@ -27,11 +27,11 @@ const SecondPage = () => {
 
 
   return (
-  <div className='w-full mx-auto'>
-    <div className="flex flex-col justify-center items-center gap-8 pt-[60px] pb-[80px] px-28 xl:px-0">
-      <div className="flex items-center gap-[2px] px-2 py-2 bg-[#F7F5F2] w-[386px] h-[85px] cursor-pointer">
+  <div className='w-full mx-auto px-3 sm:py-10 sm:px-0'>
+    <div className="flex flex-col justify-center items-center gap-8 pt-[60px] pb-[80px]  md:max-w-[720px] lg:max-w-full mx-auto lg:px-28 xl:px-0">
+      <div className="flex items-center gap-[2px] px-2 py-2 bg-[#F7F5F2] max-w-[386px] h-[85px] cursor-pointer">
         <button
-          className={`rounded-lg w-[200px] h-[73px] transition-all duration-50 delay-50 ${
+          className={`rounded-lg w-[180px] sm:w-[200px] h-[73px] transition-all duration-50 delay-50 ${
             active === 1 ? "bg-white border-[1px] border-custom-blue" : "border-none"
           }`}
           onClick={() => setActive(1)}
@@ -40,7 +40,7 @@ const SecondPage = () => {
           <span className="text-sm text-custom-blue font-ttCommonProMedium font-medium w-[138px] leading-[1.4rem] text-start">CAMP MANAGEMENT</span>
         </button>
         <button
-          className={` rounded-lg w-[170px] h-[73px] text-center transition-all duration-50 delay-50 ${
+          className={` rounded-lg w-[130px] sm:w-[170px] h-[73px] text-center transition-all duration-50 delay-50 ${
             active === 2 ? "bg-white border-[1px] border-custom-blue" : ""
           }`}
           onClick={() => setActive(2)}
@@ -51,7 +51,7 @@ const SecondPage = () => {
       </div>
     { active ===1 
         ?     
-        <div className="flex w-full xl:max-w-[741px] text-[#97A7B5] font-gilroyBold font-normal leading-[1.2rem] overflow-x-auto justify-center scrollbar-none ">
+        <div className="flex mx-auto text-nowrap max-w-inherit w-full md:max-w-[696px] lg:max-w-[741px] text-[#97A7B5] font-gilroyBold font-normal leading-[1.2rem] overflow-x-auto justify-start scrollbar-none ">
         <div className={`${ activeHeading ==="rooms"?"text-custom-heading border-b-2 border-custom-blue":"border-b-2 border-white"} py-4 px-6  transition-all delay-150 duration-150 cursor-pointer w-auto text-center`}
         onClick={()=>{setActiveHeading("rooms")}}
         >Rooms Management</div>
@@ -76,7 +76,7 @@ const SecondPage = () => {
       </div> 
       :
 
-      <div className="flex w-full xl:max-w-[1116px] justify-center text-[#97A7B5] font-gilroyBold font-normal leading-[1.2rem] overflow-x-auto scrollbar-none">
+      <div className="flex mx-auto text-nowrap max-w-inherit w-full md:max-w-[696px] lg:max-w-[741px] xl:max-w-[1116px] justify-start text-[#97A7B5] font-gilroyBold font-normal leading-[1.2rem] overflow-x-auto scrollbar-none">
       <div className={`${ activeHeading ==="transportation"?"text-custom-heading border-b-2 border-custom-blue":""} py-4 px-6 cursor-pointer w-auto text-center`}
       onClick={()=>{setActiveHeading("transportation")}}
       >Chartered Transportation</div>
@@ -104,7 +104,7 @@ const SecondPage = () => {
       {/* For setting the navigation according to the selected tab at the top */}
     { active ===1
      ?
-    <div className='xl:max-w-[1216px] w-full mx-auto'>
+    <div className='max-w-[540px] md:max-w-[696px] xl:max-w-[1216px] mx-auto'>
        {activeHeading ==="rooms" && <RoomsManagement/>}
      {activeHeading ==="frontDesk" && <Frontdesk/>}
      {activeHeading ==="housekeeping" && <Housekeeping/>}
