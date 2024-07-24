4c7f39f0d7d8e29721fe3f58aa1dddd2dad45069
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-07-24 13:02:18 +0530

    added load more option for history cards

--- src/components/Company/HistoryCards.jsx
+++ src/components/Company/HistoryCards.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const HistoryCards = () => {
+
+    const [currNum, setCurrNum] = useState(0);
+
   return (
     <div className='grid grid-cols-1 gap-10 mx-auto'>
       
@@ -69,7 +72,14 @@ const HistoryCards = () => {
                       </div>
                     </div>
                   </div>
-                </div>              
+                </div> 
+                {currNum ===0 ?
+                <div 
+                onClick={()=>setCurrNum(currNum+1)}
+                className='font-gilroyBold text-white w-full text-center underline'>
+                Load More
+                </div> :
+                <>           
                 <div className=" max-w-[580px] h-[455px]">
                   <div className="flex flex-col">
                     <h2 className="text-h2 text-custom-button font-gilroyBold">
@@ -135,95 +145,112 @@ const HistoryCards = () => {
                       </div>
                     </div>
                   </div>
-                </div>              
-                <div className=" max-w-[580px] h-[455px]">
-                  <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2015
-                    </h2>
-                    <div className="relative">
-                      <div className="absolute inset-0 bg-cover bg-history-7 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
-                      <div className="max-w-[529px] h-[400px]">
-                        <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
-                          <span className="font-gilroyBold font-normal text-white text-xl ">
-                            February 2015
-                          </span>
-                          <p className="font-ttCommonProNormal text-white">
-                            Mobile Housekeeping launches, enabling cleaners to
-                            update room status from a mobile device as they
-                            complete walkdowns or cleanings.
-                          </p>
+                </div> 
+                 
+                 {currNum ===1 ?
+                    <div 
+                    onClick={()=>setCurrNum(currNum+1)}
+                    className='font-gilroyBold text-white w-full text-center underline'>
+                    Load More
+                    </div>
+                    :
+                    <>
+                    <div className=" max-w-[580px] h-[455px]">
+                      <div className="flex flex-col">
+                        <h2 className="text-h2 text-custom-button font-gilroyBold">
+                          2015
+                        </h2>
+                        <div className="relative">
+                          <div className="absolute inset-0 bg-cover bg-history-7 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
+                          <div className="max-w-[529px] h-[400px]">
+                            <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
+                              <span className="font-gilroyBold font-normal text-white text-xl ">
+                                February 2015
+                              </span>
+                              <p className="font-ttCommonProNormal text-white">
+                                Mobile Housekeeping launches, enabling cleaners to
+                                update room status from a mobile device as they
+                                complete walkdowns or cleanings.
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </div>              
-                <div className=" max-w-[580px] h-[455px]">
-                  <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2013
-                    </h2>
-                    <div className="relative">
-                      <div className="absolute inset-0 bg-cover bg-history-8 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
-                      <div className="max-w-[529px] h-[400px]">
-                        <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
-                          <span className="font-gilroyBold font-normal text-white text-xl ">
-                            June 2013
-                          </span>
-                          <p className="font-ttCommonProNormal text-white">
-                            Construction firm implements SmartLodge at their
-                            remote camps at a hydroelectric dam construction
-                            project in Manitoba, Canada.
-                          </p>
+                    </div>              
+                    <div className=" max-w-[580px] h-[455px]">
+                      <div className="flex flex-col">
+                        <h2 className="text-h2 text-custom-button font-gilroyBold">
+                          2013
+                        </h2>
+                        <div className="relative">
+                          <div className="absolute inset-0 bg-cover bg-history-8 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
+                          <div className="max-w-[529px] h-[400px]">
+                            <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
+                              <span className="font-gilroyBold font-normal text-white text-xl ">
+                                June 2013
+                              </span>
+                              <p className="font-ttCommonProNormal text-white">
+                                Construction firm implements SmartLodge at their
+                                remote camps at a hydroelectric dam construction
+                                project in Manitoba, Canada.
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </div>              
-                <div className=" max-w-[580px] h-[455px]">
-                  <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2007
-                    </h2>
-                    <div className="relative">
-                      <div className="absolute inset-0 bg-cover bg-history-9 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
-                      <div className="max-w-[529px] h-[400px]">
-                        <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
-                          <span className="font-gilroyBold font-normal text-white text-xl ">
-                            March 2007
-                          </span>
-                          <p className="font-ttCommonProNormal text-white">
-                            The company installs the first version of SmartLodge
-                            at a remote camp serving the Canadian oil sands in
-                            Fort McMurray, Alberta, Canada.
-                          </p>
+                    </div>              
+                    <div className=" max-w-[580px] h-[455px]">
+                      <div className="flex flex-col">
+                        <h2 className="text-h2 text-custom-button font-gilroyBold">
+                          2007
+                        </h2>
+                        <div className="relative">
+                          <div className="absolute inset-0 bg-cover bg-history-9 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
+                          <div className="max-w-[529px] h-[400px]">
+                            <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
+                              <span className="font-gilroyBold font-normal text-white text-xl ">
+                                March 2007
+                              </span>
+                              <p className="font-ttCommonProNormal text-white">
+                                The company installs the first version of SmartLodge
+                                at a remote camp serving the Canadian oil sands in
+                                Fort McMurray, Alberta, Canada.
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </div>              
-                <div className=" max-w-[580px] h-[455px]">
-                  <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      1997
-                    </h2>
-                    <div className="relative">
-                      <div className="absolute inset-0 bg-cover bg-history-10 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
-                      <div className="max-w-[529px] h-[400px]">
-                        <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
-                          <span className="font-gilroyBold font-normal text-white text-xl ">
-                            June 1997
-                          </span>
-                          <p className="font-ttCommonProNormal text-white">
-                            Camps & Crew’s parent company, Innfinity Software
-                            Systems, releases a now-retired version of
-                            SmartLodge for hotels properties.
-                          </p>
+                    </div> 
+                    <div className=" max-w-[580px] h-[455px]">
+                      <div className="flex flex-col">
+                        <h2 className="text-h2 text-custom-button font-gilroyBold">
+                          1997
+                        </h2>
+                        <div className="relative">
+                          <div className="absolute inset-0 bg-cover bg-history-10 max-w-[529px] h-[400px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
+                          <div className="max-w-[529px] h-[400px]">
+                            <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
+                              <span className="font-gilroyBold font-normal text-white text-xl ">
+                                June 1997
+                              </span>
+                              <p className="font-ttCommonProNormal text-white">
+                                Camps & Crew’s parent company, Innfinity Software
+                                Systems, releases a now-retired version of
+                                SmartLodge for hotels properties.
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
+                    </div> 
+                    </>
+                    
+                    }
+                    </>
+                 }
+                            
+                            
                     </div>
-                  </div>
-                </div>    </div>
   )
 }
 
