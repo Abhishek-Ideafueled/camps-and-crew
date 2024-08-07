da0e08a44c97d3f5d05f0f5490f2797c5b7b6bf7
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-07 22:50:55 +0530

    fixed navbar disappearing and appearing

--- src/components/SmartLodge/useNavbarVisibility.jsx
+++ src/components/SmartLodge/useNavbarVisibility.jsx
@@ -29,13 +29,35 @@ const useNavbarVisibility = () => {
           } else if (!scrollingDown && !entry.isIntersecting) {
             setIsVisible(false);
           }
-        } else if (entry.target.id === 'features') {
+        }
+        else if (entry.target.id === 'reception') {
+            if (entry.isIntersecting) {
+              setIsVisible(true);
+            } 
+          }
+          else if (entry.target.id === 'housekeeping') {
+            if (entry.isIntersecting) {
+              setIsVisible(true);
+            } 
+          }
+          else if (entry.target.id === 'reports') {
+            if (entry.isIntersecting) {
+              setIsVisible(true);
+            } 
+          } else if (entry.target.id === 'systemIntegrations') {
           if (entry.isIntersecting) {
             setIsVisible(true);
-          } else if (scrollingDown && !entry.isIntersecting) {
+          } else if (!scrollingDown && !entry.isIntersecting) {
             setIsVisible(false);
           }
         }
+        else if (entry.target.id === 'features') {
+            if (entry.isIntersecting) {
+              setIsVisible(true);
+            } else if (scrollingDown && !entry.isIntersecting) {
+              setIsVisible(false);
+            }
+          }
       });
     };
 
@@ -44,13 +66,25 @@ const useNavbarVisibility = () => {
     });
 
     const firstSection = document.getElementById('roomsManagement');
+    const secondSection = document.getElementById('reception');
+    const thirdSection = document.getElementById('housekeeping');
+    const fourthSection = document.getElementById('reports');
+    const fifthSection = document.getElementById('systemIntegrations');
     const lastSection = document.getElementById('features');
 
     if (firstSection) observer.observe(firstSection);
+    if (secondSection) observer.observe(secondSection);
+    if (thirdSection) observer.observe(thirdSection);
+    if (fourthSection) observer.observe(fourthSection);
+    if (fifthSection) observer.observe(fifthSection);
     if (lastSection) observer.observe(lastSection);
 
     return () => {
       if (firstSection) observer.unobserve(firstSection);
+      if (secondSection) observer.unobserve(secondSection);
+      if (thirdSection) observer.unobserve(thirdSection);
+      if (fourthSection) observer.unobserve(fourthSection);
+      if (fifthSection) observer.unobserve(fifthSection);
       if (lastSection) observer.unobserve(lastSection);
     };
   }, [scrollingDown]);
