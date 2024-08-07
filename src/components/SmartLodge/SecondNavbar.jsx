4a5e79473b83bf0f08a28d26e8e361cae4f9a11d
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-07 14:10:23 +0530

    testing

---
+++ src/components/SmartLodge/SecondNavbar.jsx
@@ -0,0 +1,11 @@
+import './SecondNavbar.css';
+
+const SecondNavbar = () => {
+  return (
+    <div className="main-container mx-auto fixed top-0 z-30 bg-custom-back text-white">
+
+    </div>
+  )
+}
+
+export default SecondNavbar
