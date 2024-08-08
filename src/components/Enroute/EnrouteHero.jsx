530b82319445af3fd5298f607e526741b04f639f
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-08 14:46:46 +0530

    enroute section on hold for now

---
+++ src/components/Enroute/EnrouteHero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const EnrouteHero = () => {
+  return (
+    <div className='w-full bg-custom-back'>
+        <div className='main-container mx-auto py-10 lg:py-20 h-[100vh]'>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 w-full items-center">
+            
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default EnrouteHero
--- src/components/Enroute/EnrouteMain.jsx
+++ src/components/Enroute/EnrouteMain.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import EnrouteHero from './EnrouteHero';
 
 const EnrouteMain = () => {
   return (
     <div className='w-full mt-[76px] lg:mt-[106px] overflow-hidden'>
-          
+          <EnrouteHero/>
     </div>
   )
 }
