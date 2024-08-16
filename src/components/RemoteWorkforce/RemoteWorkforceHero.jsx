0984817306b9d5ca069a65423fa3237bc147cee1
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-16 16:15:10 +0530

    completed first section of remote workforce

--- src/components/RemoteWorkforce/RemoteWorkforceHero.jsx
+++ src/components/RemoteWorkforce/RemoteWorkforceHero.jsx
@@ -1,7 +1,95 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Slider from 'react-slick';
+import './RemoteWorkforceMain.css';
 
 const RemoteWorkforceHero = () => {
+
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 10000,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    autoplay: true,
+    vertical: true,
+    autoplaySpeed:0,
+    pauseOnHover:true,
+     adaptiveHeight:false,
+     cssEase: 'linear',
+     arrows:false,
+      responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1.2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1.2,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow:1.6,
+        },
+      },
+      {
+        breakpoint: 2200,
+        settings: {
+          slidesToShow:1.6 ,
+        },
+      },
+      
+    ],
+  };
+  const settings2 = {
+    dots: false,
+    infinite: true,
+    speed: 10000,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    autoplay: true,
+    vertical: true,
+    autoplaySpeed:0,
+    pauseOnHover:true,
+     adaptiveHeight:false,
+     cssEase: 'linear',
+     arrows:false,
+      responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1.2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1.2,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow:1.6,
+        },
+      },
+      {
+        breakpoint: 2200,
+        settings: {
+          slidesToShow:1.6 ,
+        },
+      },
+      
+    ],
+    rtl:true
+  };
+
   return (
     <div className='w-full bg-[#f7f5f2]'>
         <div className='main-container mx-auto'>
@@ -22,6 +110,74 @@ const RemoteWorkforceHero = () => {
             <Link>
                     <div className='btn-orange w-[203px] h-[64px] py-[19px] px-10 text-nowrap rounded'>CONTACT SALES</div>
             </Link>
+        </div>
+        </div>
+        <div className='remote-sliders'>
+        <div className='remote-upwards flex items-center justify-center gap-3 lg:gap-7'>
+        <div className="remote-upwards w-1/2 h-full">
+              <Slider {...settings}>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="/rw-slider-01.png"
+                    alt="woman-backpack-looks-onto-plane-airport"
+                  />
+                </div>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="rw-slider-02.png"
+                    alt="people-seated-rows-commercial-plane"
+                  />
+                </div>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover "
+                    src="rw-slider-03.png"
+                    alt="man-rolls-suitcase-baggage-carousel-area"
+                  />
+                </div>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="rw-slider-04.png"
+                    alt="fifo-workers-take-ferry-remote-project"
+                  />
+                </div>
+              </Slider>
+            </div>
+            <div className="remote-downwards w-1/2 h-full">
+              <Slider {...settings2}>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="/rw-slider-05.png"
+                    alt="parking-lot-workforce-camp"
+                  />
+                </div>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="/rw-slider-06.png"
+                    alt="fifo-worker-driving-to-camp-dido"
+                  />
+                </div>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="/rw-slider-07.png"
+                    alt="man-entering-hotel-room"
+                  />
+                </div>
+                <div className="max-w-[246px] md:max-w-[156px] lg:max-w-[216px] xl:max-w-[288px]">
+                  <img
+                    className="rounded-2xl h-[315px] object-cover"
+                    src="/rw-slider-06.png"
+                    alt="hotel-bed-open-door-sunny"
+                  />
+                </div>
+              </Slider>
+            </div>
         </div>
         </div>
         </div>
--- src/components/RemoteWorkforce/RemoteWorkforceMain.jsx
+++ src/components/RemoteWorkforce/RemoteWorkforceMain.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import RemoteWorkforceHero from './RemoteWorkforceHero';
 
 const RemoteWorkforceMain = () => {
+
   return (
     <div className='mt-[76px] lg:mt-[106px] w-full overflow-hidden'>
       <RemoteWorkforceHero/>
