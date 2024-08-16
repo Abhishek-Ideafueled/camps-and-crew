1b8afcc427e6207bbd2794d6079949209b42cef7
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-16 16:59:14 +0530

    completed logos section of remote workforce

---
+++ src/components/RemoteWorkforce/RWLogos.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const RWLogos = () => {
+  return (
+    <div className='w-full'>
+        <div className='main-container mx-auto py-10 lg:py-20 flex flex-col gap-[30px] lg:gap-[60px]'>
+            <h3 className='font-gilroyBold font-normal text-xl leading-6 md:text-[22px] md:leading-[27px] lg:text-2xl lg:leading-[28.8px]'>
+            Trusted by Camp Services Companies Globally
+            </h3>
+            <div className='grid grid-cols-3 lg:grid-cols-7 gap-4 lg:gap-20'>
+                <div className='h-[60px] flex justify-center'>
+            <img src="/civeo-color.png" alt="" className='h-[60px] object-contain' />
+                </div>
+                <div className='h-[60px] '>
+                <img src="/petroplex-logo.png" alt=""  className='mx-auto' />
+                </div>
+                <div className='h-[60px] flex justify-center'>
+                <img src="/permian-lodging.png" alt=""  className='h-[60px] object-contain' />
+                </div>
+                <div className='h-[60px] flex justify-center'>
+                <img src="/sodexo-logo.png" alt=""   className='h-[60px] object-contain'/>
+                </div>
+                <div className='h-[60px] flex justify-center'>
+                <img src="/denali-universal-services.png" alt=""  className='h-[60px] object-contain' />
+                </div>
+                <div className='h-[60px] flex justify-center'>
+                <img src="/cotton-logistics.png" alt=""  className='h-[60px] object-contain'/>
+                </div>
+                <div className='h-[60px] col-span-3 flex justify-center mt-5 lg:mt-0 lg:col-span-1'>
+                <img src="/right-choice-camps-catering.png" alt=""  className='h-[60px] object-contain' />
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default RWLogos
--- src/components/RemoteWorkforce/RemoteWorkforceMain.jsx
+++ src/components/RemoteWorkforce/RemoteWorkforceMain.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import RemoteWorkforceHero from './RemoteWorkforceHero';
+import RWLogos from './RWLogos';
 
 const RemoteWorkforceMain = () => {
 
   return (
     <div className='mt-[76px] lg:mt-[106px] w-full overflow-hidden'>
       <RemoteWorkforceHero/>
+      <RWLogos/>
     </div>
   )
 }
