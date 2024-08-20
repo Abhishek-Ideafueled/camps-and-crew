a24a3defff5b986c49f9e998513df27c17c665d2
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-20 11:51:18 +0530

    testing year card slider

--- src/components/Testing.jsx
+++ src/components/Testing.jsx
@@ -5,6 +5,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/controller';
 import { Navigation, Pagination, Controller,Scrollbar } from 'swiper/modules';
+import './Testing.css';
 
 const Testing = () => {
 
@@ -19,56 +20,55 @@ const Testing = () => {
     <div className='mt-[200px] main-container mx-auto'>
      <div className='w-full flex flex-col items-center'>
      <div className="h-full w-full relative history-card overflow-hidden no-scrollbar ">
-            <div className="absolute top-0 bg-[#053347] font-gilroyBold text-custom-button text-h2 leading-[3rem] z-10 h-[60px] w-28 flex justify-center items-center">
+            <div className="absolute top-0 font-gilroyBold text-custom-heading text-h2 leading-[3rem] z-10 h-[60px] w-28 flex justify-center items-center">
               {years.filter((v, i) => (v ? i === currIndex : ""))}
             </div>
             <Swiper
               onSwiper={(swiper) => {
                 horizontalSwiperRef.current = swiper;
               }}
-              spaceBetween={50}
+              spaceBetween={10}
               scrollbar={{ draggable: true }}
-              slidesPerView={3}
+              slidesPerView="auto"
+              // slidesPerGroup="auto"
               // pagination={{
               //   clickable: true,
               // }}
               modules={[Navigation, Controller, Pagination,Scrollbar]}
               navigation={false}
-              className="h-full"
+              className="h-full mt-20"
               onSlideChange={(swiper) => {
                 setCurrIndex(swiper.activeIndex);
               }}
               // controller={{ control: verticalSwiperRef.current }}
-              breakpoints={{
-                767: {
-                  slidesPerView: 2.1,
-                  spaceBetween: 10,
-                },
-                1064: {
-                  slidesPerView: 2.5,
-                  spaceBetween: 10,
-                },
-                1240: {
-                  slidesPerView: 3,
-                  spaceBetween: 10,
-                },
-                1400: {
-                  slidesPerView: 4,
-                  spaceBetween: 10,
-                },
-                1600: {
-                  slidesPerView: 4,
-                  spaceBetween: 10,
-                },
-              }}
+              // breakpoints={{
+              //   767: {
+              //     slidesPerView: 2.1,
+              //     spaceBetween: 10,
+              //   },
+              //   1064: {
+              //     slidesPerView: 2.5,
+              //     spaceBetween: 10,
+              //   },
+              //   1240: {
+              //     slidesPerView: 3,
+              //     spaceBetween: 10,
+              //   },
+              //   1400: {
+              //     slidesPerView: 4,
+              //     spaceBetween: 10,
+              //   },
+              //   1600: {
+              //     slidesPerView: 4,
+              //     spaceBetween: 10,
+              //   },
+              // }}
             >
           
               <SwiperSlide key={0}>
-                <div className="slider-outer w-auto h-auto">
+                <div className="slider-outer flex gap-4 w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2023
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-1 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -85,14 +85,48 @@ const Testing = () => {
                       </div>
                     </div>
                   </div>
+                  <div className="flex flex-col">
+                   
+                   <div className="relative">
+                     <div className="absolute bg-cover bg-history-1 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
+                     <div className="w-full max-w-[340px] h-[405px]">
+                       <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
+                         <span className="font-gilroyBold font-normal text-white text-2xl ">
+                           August 2023
+                         </span>
+                         <p className="font-ttCommonProNormal text-white text-xl">
+                           The company launches the ability for charter plane
+                           pilots and bus drivers to check workers in as they
+                           board planes or buses with EnRoute.
+                         </p>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+                 <div className="flex flex-col">
+                   
+                   <div className="relative">
+                     <div className="absolute bg-cover bg-history-1 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
+                     <div className="w-full max-w-[340px] h-[405px]">
+                       <div className="relative flex flex-col p-6 justify-end gap-4 w-full h-full">
+                         <span className="font-gilroyBold font-normal text-white text-2xl ">
+                           August 2023
+                         </span>
+                         <p className="font-ttCommonProNormal text-white text-xl">
+                           The company launches the ability for charter plane
+                           pilots and bus drivers to check workers in as they
+                           board planes or buses with EnRoute.
+                         </p>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
                 </div>
               </SwiperSlide>
               <SwiperSlide key={1}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2022
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-2 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -114,9 +148,7 @@ const Testing = () => {
               <SwiperSlide key={2}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2021
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-3 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -138,9 +170,7 @@ const Testing = () => {
               <SwiperSlide key={3}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2018
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-4 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -162,9 +192,7 @@ const Testing = () => {
               <SwiperSlide key={4}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2017
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-5 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -186,9 +214,7 @@ const Testing = () => {
               <SwiperSlide key={5}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2016
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-6 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -210,9 +236,7 @@ const Testing = () => {
               <SwiperSlide key={6}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2015
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-7 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -234,9 +258,7 @@ const Testing = () => {
               <SwiperSlide key={7}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2013
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-8 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -258,9 +280,7 @@ const Testing = () => {
               <SwiperSlide key={8}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      2007
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-9 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
@@ -282,9 +302,7 @@ const Testing = () => {
               <SwiperSlide key={9}>
                 <div className="slider-outer w-auto h-auto">
                   <div className="flex flex-col">
-                    <h2 className="text-h2 text-custom-button font-gilroyBold">
-                      1997
-                    </h2>
+                   
                     <div className="relative">
                       <div className="absolute bg-cover bg-history-10 w-full max-w-[340px] h-[405px] bottom-0 rounded-2xl border-[#5BC0EC] border-[1px]"></div>
                       <div className="w-full max-w-[340px] h-[405px]">
