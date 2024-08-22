0ae7be296ebbaa0fb1895bdc7e5bf38b05f48694
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-22 15:01:01 +0530

    fixed  rw case study cards

--- src/components/RemoteWorkforce/RwCaseStudy.jsx
+++ src/components/RemoteWorkforce/RwCaseStudy.jsx
@@ -188,9 +188,9 @@ const RwCaseStudy = () => {
           </span>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-10">
-        <div className="flex flex-col border-custom-blue hover:bg-opacity-50 border hover:border-transparent bg-custom-heading hover:bg-rw-case-3 h-full lg:min-h-[480px] p-10 relative justify-between rw-card-container">
+        <div className="flex flex-col border-custom-blue hover:bg-opacity-50 border hover:border-transparent bg-custom-heading hover:bg-rw-case-3 h-full min-h-[355px] lg:min-h-[480px] px-4 py-8 lg:p-10 relative justify-between rw-card-container">
             <div className="absolute inset-0 w-full h-full bg-[#5BC0ED99] opacity-0 bg-grad"></div>
-             <div className='flex flex-col h-full justify-between'>
+             <div className='flex flex-col h-full justify-between part-white'>
               <span className='h-[50px]'>
                 <img className='object-cover' src="/white-sodexo.png" alt="white-sodexo" />
               </span>
@@ -201,7 +201,7 @@ const RwCaseStudy = () => {
             </h4>
             <Link
               to="#"
-              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base text-white z-10 max-w-max"
+              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base text-white opacity-0 max-w-max"
             >
               <p>READ STORY</p>
               <span>
@@ -220,10 +220,33 @@ const RwCaseStudy = () => {
               </span>
             </Link>
             </div>
+            <h4 className='card-heading dark-part text-[#060F1F] absolute inset-0 px-4 lg:px-10 top-[10%] opacity-0' >
+            Civeo Speeds Up Check-In and Streamlines Camp Operations Across North America with SmartLodge
+            </h4>
+            <Link
+              to="#"
+              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base absolute bottom-[10%] dark-part text-white z-10 max-w-max"
+            >
+              <p>READ STORY</p>
+              <span>
+                <svg
+                  width="22"
+                  height="12"
+                  viewBox="0 0 22 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.199219 5.8258C0.369099 5.3032 0.727999 5.1221 1.25116 5.1233C7.03515 5.1325 12.8197 5.1291 18.6037 5.1291C18.687 5.1291 18.7702 5.1291 18.9047 5.1291C18.8203 5.0385 18.7669 4.9787 18.7112 4.9206C17.7999 3.9835 16.8891 3.04637 15.9767 2.111C15.7292 1.85726 15.6482 1.55418 15.7455 1.21452C15.8361 0.899238 16.0515 0.697768 16.3643 0.623448C16.6877 0.546808 16.9555 0.659448 17.1861 0.896918C18.2066 1.94668 19.2293 2.99412 20.2508 4.0427C20.6294 4.4317 21.0041 4.8248 21.3877 5.208C21.5672 5.3874 21.7213 5.5785 21.8012 5.8258C21.8012 5.9419 21.8012 6.058 21.8012 6.1742C21.7207 6.4209 21.5649 6.6102 21.3877 6.7914C19.9898 8.2209 18.5964 9.655 17.2002 11.0862C16.8278 11.4683 16.344 11.5037 15.993 11.1809C15.6184 10.8365 15.6071 10.2728 15.9784 9.889C16.8869 8.9495 17.7999 8.0142 18.7129 7.0794C18.7669 7.0242 18.8355 6.9847 18.8974 6.9377C18.8867 6.915 18.8754 6.8924 18.8647 6.8703C18.7849 6.8703 18.705 6.8703 18.6251 6.8703C12.8344 6.8703 7.04303 6.8668 1.25229 6.8761C0.728559 6.8773 0.369669 6.6961 0.199219 6.1742C0.199219 6.058 0.199219 5.9419 0.199219 5.8258Z"
+                    fill="white"
+                  />
+                </svg>
+              </span>
+            </Link>
           </div>
-          <div className="flex flex-col border-custom-blue border hover:border-transparent bg-custom-heading hover:bg-rw-case-1 h-full lg:min-h-[480px] p-10 relative justify-between rw-card-container">
+          <div className="flex flex-col border-custom-blue border hover:border-transparent bg-custom-heading hover:bg-rw-case-1 h-full min-h-[355px] lg:min-h-[480px] px-4 py-8 lg:p-10 relative justify-between rw-card-container">
             <div className="absolute inset-0 w-full h-full bg-[#5BC0ED99] opacity-0 bg-grad"></div>
-            <div className='flex flex-col justify-between h-full'>
+            <div className='flex flex-col justify-between h-full part-white'>
             <span className='w-[98px] h-[50px]'>
               <img src="/white-civeo.png" alt="white-civeo" />
               </span>           
@@ -232,7 +255,7 @@ const RwCaseStudy = () => {
             </h4>
             <Link
               to="#"
-              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base text-white z-10 max-w-max"
+              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base opacity-0 text-white max-w-max"
             >
               <p>READ STORY</p>
               <span>
@@ -251,10 +274,33 @@ const RwCaseStudy = () => {
               </span>
             </Link>
             </div>
+            <h4 className='card-heading dark-part text-[#060F1F] absolute inset-0 px-4 lg:px-10 top-[10%] opacity-0' >
+            Civeo Speeds Up Check-In and Streamlines Camp Operations Across North America with SmartLodge
+            </h4>
+            <Link
+              to="#"
+              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base absolute bottom-[10%] dark-part text-white z-10 max-w-max"
+            >
+              <p>READ STORY</p>
+              <span>
+                <svg
+                  width="22"
+                  height="12"
+                  viewBox="0 0 22 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.199219 5.8258C0.369099 5.3032 0.727999 5.1221 1.25116 5.1233C7.03515 5.1325 12.8197 5.1291 18.6037 5.1291C18.687 5.1291 18.7702 5.1291 18.9047 5.1291C18.8203 5.0385 18.7669 4.9787 18.7112 4.9206C17.7999 3.9835 16.8891 3.04637 15.9767 2.111C15.7292 1.85726 15.6482 1.55418 15.7455 1.21452C15.8361 0.899238 16.0515 0.697768 16.3643 0.623448C16.6877 0.546808 16.9555 0.659448 17.1861 0.896918C18.2066 1.94668 19.2293 2.99412 20.2508 4.0427C20.6294 4.4317 21.0041 4.8248 21.3877 5.208C21.5672 5.3874 21.7213 5.5785 21.8012 5.8258C21.8012 5.9419 21.8012 6.058 21.8012 6.1742C21.7207 6.4209 21.5649 6.6102 21.3877 6.7914C19.9898 8.2209 18.5964 9.655 17.2002 11.0862C16.8278 11.4683 16.344 11.5037 15.993 11.1809C15.6184 10.8365 15.6071 10.2728 15.9784 9.889C16.8869 8.9495 17.7999 8.0142 18.7129 7.0794C18.7669 7.0242 18.8355 6.9847 18.8974 6.9377C18.8867 6.915 18.8754 6.8924 18.8647 6.8703C18.7849 6.8703 18.705 6.8703 18.6251 6.8703C12.8344 6.8703 7.04303 6.8668 1.25229 6.8761C0.728559 6.8773 0.369669 6.6961 0.199219 6.1742C0.199219 6.058 0.199219 5.9419 0.199219 5.8258Z"
+                    fill="white"
+                  />
+                </svg>
+              </span>
+            </Link>
           </div>
-          <div className="flex flex-col border-custom-blue border hover:border-transparent bg-custom-heading hover:bg-rw-case-2 h-full lg:min-h-[480px] p-10 relative justify-between rw-card-container">
+          <div className="flex flex-col border-custom-blue border hover:border-transparent bg-custom-heading hover:bg-rw-case-2 h-full min-h-[355px] lg:min-h-[480px] px-4 py-8 lg:p-10 relative justify-between rw-card-container">
             <div className="absolute inset-0 w-full h-full bg-[#5BC0ED99] opacity-0 bg-grad"></div>
-            <div className='flex flex-col justify-between h-full'>
+            <div className='flex flex-col justify-between h-full opacity-100 part-white'>
             <span className='h-[50px]'>
               <img
               className='object-cover'
@@ -265,7 +311,7 @@ const RwCaseStudy = () => {
             </h4>
             <Link
               to="#"
-              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base text-white z-10 max-w-max"
+              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base opacity-0 text-white max-w-max"
             >
               <p>READ STORY</p>
               <span>
@@ -284,6 +330,29 @@ const RwCaseStudy = () => {
               </span>
             </Link>
             </div>
+            <h4 className='card-heading dark-part text-[#060F1F] absolute inset-0 px-4 lg:px-10 top-[10%] opacity-0' >
+            Edmonton-Based Right Choice Camps & Catering Streamlines Operations and Billing for Network of Remote Lodges
+            </h4>
+            <Link
+              to="#"
+              className="flex items-center gap-2 font-ttCommonProRegular font-semibold text-base absolute bottom-[10%] dark-part text-white z-10 max-w-max"
+            >
+              <p>READ STORY</p>
+              <span>
+                <svg
+                  width="22"
+                  height="12"
+                  viewBox="0 0 22 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.199219 5.8258C0.369099 5.3032 0.727999 5.1221 1.25116 5.1233C7.03515 5.1325 12.8197 5.1291 18.6037 5.1291C18.687 5.1291 18.7702 5.1291 18.9047 5.1291C18.8203 5.0385 18.7669 4.9787 18.7112 4.9206C17.7999 3.9835 16.8891 3.04637 15.9767 2.111C15.7292 1.85726 15.6482 1.55418 15.7455 1.21452C15.8361 0.899238 16.0515 0.697768 16.3643 0.623448C16.6877 0.546808 16.9555 0.659448 17.1861 0.896918C18.2066 1.94668 19.2293 2.99412 20.2508 4.0427C20.6294 4.4317 21.0041 4.8248 21.3877 5.208C21.5672 5.3874 21.7213 5.5785 21.8012 5.8258C21.8012 5.9419 21.8012 6.058 21.8012 6.1742C21.7207 6.4209 21.5649 6.6102 21.3877 6.7914C19.9898 8.2209 18.5964 9.655 17.2002 11.0862C16.8278 11.4683 16.344 11.5037 15.993 11.1809C15.6184 10.8365 15.6071 10.2728 15.9784 9.889C16.8869 8.9495 17.7999 8.0142 18.7129 7.0794C18.7669 7.0242 18.8355 6.9847 18.8974 6.9377C18.8867 6.915 18.8754 6.8924 18.8647 6.8703C18.7849 6.8703 18.705 6.8703 18.6251 6.8703C12.8344 6.8703 7.04303 6.8668 1.25229 6.8761C0.728559 6.8773 0.369669 6.6961 0.199219 6.1742C0.199219 6.058 0.199219 5.9419 0.199219 5.8258Z"
+                    fill="white"
+                  />
+                </svg>
+              </span>
+            </Link>
           </div>
         </div>
         <div className='flex w-full justify-center'>
