fb4191ed5dc4ea26d0c55a2c581c062764002d0a
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-23 11:00:20 +0530

    completed mining hero page

--- src/components/Mining/MiningHero.jsx
+++ src/components/Mining/MiningHero.jsx
@@ -19,8 +19,8 @@ const MiningHero = () => {
   };
 
   return (
-    <div className='w-full bg-custom-heading'>
-        <div className='main-container mx-auto py-10 lg:pt-[120px] flex flex-col items-center'>
+    <div className='w-full bg-custom-heading pb-10 lg:pb-20'>
+        <div className='main-container mx-auto pt-[70px] lg:pt-[120px] flex flex-col items-center pb-12'>
         <div className='flex flex-col gap-6 items-center'>
         <span className='font-ttCommonProRegular font-medium text-[#189AD2] text-sm'>
             | MINING |
@@ -32,11 +32,13 @@ const MiningHero = () => {
         Designed to meet the unique demands of remote mining projects, we leverage <br className='hidden xl:block' /> years of experience to build software solutions that streamline the way mining <br className='hidden xl:block' /> companies manage transportation and housing for their workforces.
             </div>
         </div>
-    <Link className='max-w-max ' href="#demoForm" onClick={handleClickScroll}>
+    <Link className='max-w-max mt-10' href="#demoForm" onClick={handleClickScroll}>
         <div className='btn-orange flex items-center justify-center py-[14px] px-4'>
       CONTACT SALES
         </div></Link>
-        <div className="case-studies-splide">
+       
+        </div>
+        <div className="mining-hero-splide">
         <Splide
           options={{
             type: "loop",
@@ -48,47 +50,56 @@ const MiningHero = () => {
             speed: 0.5,
             breakpoints: {
               2024: {
-                perPage: 6,
+                perPage: 5.5,
+              },
+              1400: {
+                perPage: 5,
               },
               1280: {
                 perPage: 4,
               },
               1024: {
-                perPage: 4,
+                perPage: 3.5,
+              },
+              900: {
+                perPage: 3,
               },
               767: {
-                perPage: 2,
+                perPage: 2.5,
               },
               640: {
                 perPage: 2,
               },
+              500: {
+                perPage: 1.5,
+              },
             },
             autoScroll: {
-              speed: 1,
+              speed: 0.6,
               pauseOnHover: true,
               pauseOnFocus: true,
             },
           }}
           extensions={{ AutoScroll }}
-          aria-label="My Favorite Images"
+          aria-label="Mining hero images"
           // hasTrack={false}
         >
           <SplideSlide>
             <div className="single-slide">
-              <img className="" src="white-civeo.png" alt="Image 1" />
+              <img className="rounded-2xl object-cover" src="worker-accommodation-bedroom-camp.jpg" alt="Mining camp bedroom with bed, desk and armoire" />
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className="flex justify-center items-center mt-5">
-              <img className="" src="white-petroplex.png" alt="Image 2" />
+            <div className="single-slide">
+              <img className="rounded-2xl object-cover" src="haul-trucks-mine.jpg" alt="Haul trucks at open pit mine" />
             </div>
           </SplideSlide>
           <SplideSlide>
             <div className="single-slide">
               <img
-                className=""
-                src="/white-b2gold.png"
-                alt="b2gold logo white"
+                className="rounded-2xl object-cover"
+                src="/mining-worker-directs-haul-truck.jpg"
+                alt="Worker radios colleague at mine site with dump truck in background"
               />
             </div>
           </SplideSlide>
@@ -96,27 +107,27 @@ const MiningHero = () => {
           <SplideSlide>
             <div className="single-slide">
               <img
-                className=" "
-                src="/white-anokiigamig.png"
-                alt="white-anokiigamig logo"
+                className="rounded-2xl object-cover"
+                src="/fifo-workers-dining.jpg"
+                alt="Workers wearing PPE eat dinner at mine camp"
               />
             </div>
           </SplideSlide>
           <SplideSlide>
             <div className="single-slide">
               <img
-                className=""
-                src="/white-agincourt-resources.png"
-                alt="white-agincourt-resources logo"
+                className="rounded-2xl object-cover"
+                src="/remote-mining-camp-aerial-grassy-field.jpg"
+                alt="aerial perspective of grasslands and remote mining lodge"
               />
             </div>
           </SplideSlide>
           <SplideSlide>
             <div className="single-slide">
               <img
-                className=" "
-                src="/white-civeo.png"
-                alt="white-civeo logo"
+                className="rounded-2xl object-cover"
+                src="/female-fifo-workers-seated-plane-window3.jpg"
+                alt="FIFO workers seated on airplane"
               />
             </div>
           </SplideSlide>
@@ -124,34 +135,23 @@ const MiningHero = () => {
           <SplideSlide>
             <div className="single-slide">
               <img
-                className=""
-                src="/white-denali-universal-services.png"
-                alt="white denali universal services logo"
+                className="rounded-2xl object-cover"
+                src="/workers-board-chartered-bus.jpg"
+                alt="Fly in fly out workers board white bus"
               />
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className="single-slide px-5">
+            <div className="single-slide">
               <img
-                className=""
-                src="/white-sodexo.png"
-                alt="white-sodexo logo"
+                className="rounded-2xl object-cover"
+                src="/worker-checks-in-fifo-camp.jpg"
+                alt="Project personnel walks to room at mining camp"
               />
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="single-slide"> 
-            <img
-                  className=""
-                  src="/white-permian-lodging.png"
-                  alt="white permian lodging logo"
-                />
             </div>
           </SplideSlide>
         </Splide>
       </div>
-        </div>
-      
     </div>
   )
 }
