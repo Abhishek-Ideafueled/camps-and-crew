87a0c6813357f212c762a2ccc381f183b04e1ab9
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-08-28 21:02:08 +0530

    first draft of sodexo hero case study

--- src/components/CaseStudiesInner/CsSodexoDrives/SodexoDrivesHero.jsx
+++ src/components/CaseStudiesInner/CsSodexoDrives/SodexoDrivesHero.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 const SodexoDrivesHero = () => {
   return (
-    <div className='w-full bg-case-studies-innner py-10 lg:py-20 lg:max-h-[595px] relative'>
-      {/* <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 h-full main-container mx-auto lg:max-w-full lg:px-0'> */}
+    <div className='w-full bg-case-studies-innner py-10 lg:py-20 max-h-max relative'>
         <div className='absolute -left-10 max-w-[567px] h-full order-last lg:order-first'>       
-        <img className='w-full h-full rounded-r-2xl object-cover'
+        <img className='w-full h-[482px] rounded-r-2xl object-cover'
         src="/sodexo-sella-ness-camp-entrance2.png" 
         alt="sodexo-sella-ness-camp-entrance" />
         </div>
 
 
-        <div className='flex flex-col lg:pt-5 main-container w-full pl-28 '>
+        <div className='flex flex-col lg:pt-5 main-container w-full mx-auto max-w-[800px] pl-56 '>
         <div className='flex flex-col gap-[60px] lg:h-[344px]'>
         <span>
           <img src="/sodexo-logo2.png" alt="sodexo-logo" />
@@ -40,7 +39,9 @@ const SodexoDrivesHero = () => {
           </div>
         </div>
         </div>
-        <div className='lg:grid grid-cols-1 md:grid-cols-3 justify-between hidden pt-10'>
+       
+        </div>
+        <div className='lg:grid grid-cols-1 md:grid-cols-3 justify-between hidden pt-[60px] main-container mx-auto max-w-[800px] pl-56'>
           <div className='flex flex-col'>
              <span className='font-gilroyBold text-custom-heading text-xl'>PROJECT TYPES</span>
              <span className='font-ttCommonProRegular font-medium text-custom-heading text-base leading-[25.6px]'>
@@ -60,8 +61,6 @@ const SodexoDrivesHero = () => {
              </span>
           </div>
         </div>
-        </div>
-      {/* </div> */}
     </div>
   )
 }
