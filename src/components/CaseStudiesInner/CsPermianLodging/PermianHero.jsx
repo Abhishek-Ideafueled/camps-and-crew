75077dca889a8d9e570b4278c58c1b7518873f96
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-09-23 16:43:54 +0530

    fixed alignment in permian lodging

--- src/components/CaseStudiesInner/CsPermianLodging/PermianHero.jsx
+++ src/components/CaseStudiesInner/CsPermianLodging/PermianHero.jsx
@@ -21,7 +21,7 @@ const PermianHero = () => {
             <h3 className="cs-main-heading py-8 lg:py-0 lg:min-h-[230px] flex items-center">
             Permian Lodging Adopts SmartLodge to <br className='hidden lg:block'/> Automate Billing and Enhance the Guest Experience
             </h3>
-            <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-4 md:gap-6 xl:gap-[50px]">
               <div className="flex flex-col">
                 <span className="cs-nums text-custom-button">30%</span>
                 <span className="font-ttCommonProNormal font-semibold text-white font-base leading-[22.4px]">
@@ -46,10 +46,15 @@ const PermianHero = () => {
       </div>
     </div>
     <div className="w-full bg-[#5BC0ED] py-10 h-[240px] lg:h-[140px]">
-      <div className="main-container hidden lg:flex mx-auto gap-8">
-        <div className="cs-back-image"></div>
+      <div className="main-container hidden lg:flex mx-auto xl:gap-8">
+        <div className="cs-back-image">
+          <div className='w-[526px]'>
 
-        <div className="lg:pl-[28px] grid grid-cols-1 md:grid-cols-3  main-container max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] scLg:max-w-[700px] scLg:pl-10">
+          </div>
+        </div>
+
+        <div className="flex max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] items-center self-start">
+        <div className=" grid grid-cols-1 md:grid-cols-3 justify-between gap-4 md:gap-6 xl:gap-[50px]">
           <div className="flex flex-col">
             <span className="font-gilroyBold text-custom-heading text-xl">
               PROJECT TYPES
@@ -74,6 +79,7 @@ const PermianHero = () => {
             Workforce Accommodations
             </span>
           </div>
+        </div>
         </div>
       </div>
     </div>
