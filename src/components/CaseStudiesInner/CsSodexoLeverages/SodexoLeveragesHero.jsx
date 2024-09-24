cb6a045fd326aef7c347ce5d352292100e8a6ad7
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-09-24 12:22:54 +0530

    fixed sodexo leverages image and grid alignment

--- src/components/CaseStudiesInner/CsSodexoLeverages/SodexoLeveragesHero.jsx
+++ src/components/CaseStudiesInner/CsSodexoLeverages/SodexoLeveragesHero.jsx
@@ -5,7 +5,7 @@ const SodexoLeveragesHero = () => {
     <div className="w-full max-h-max">
       <div className="w-full bg-custom-back h-[700px] smalls:h-[700px] md:h-[600px] lg:h-[450px] pt-10">
         <div className="flex flex-col-reverse lg:gap-8 lg:flex-row main-container w-full mx-auto">
-          <div className="relative cs-back-image">
+          <div className="relative sodexo-lev cs-back-image">
             <div className="lg:absolute scLg:static scLg:w-[526px] cs-img-container min-w-[300px] lg:w-[1000px] h-[300px] md:min-h-[350px] lg:h-[500px] top-[0px] bottom-0 right-0 rounded-2xl bg-no-repeat"></div>
           </div>
 
@@ -21,7 +21,7 @@ const SodexoLeveragesHero = () => {
               <h3 className="cs-main-heading py-8 lg:py-0 lg:min-h-[230px] flex items-center">
               Sodexo Leverages SmartLodge to Lift Room Occupancy and Drive Efficiencies at Cedar Valley Lodge
               </h3>
-              <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-4 md:gap-6">
                 <div className="flex flex-col">
                   <span className="cs-nums text-custom-button">98%</span>
                   <span className="font-ttCommonProNormal font-semibold text-white font-base leading-[22.4px]">
@@ -46,10 +46,12 @@ const SodexoLeveragesHero = () => {
         </div>
       </div>
       <div className="w-full bg-[#5BC0ED] py-10 h-[240px] sm:h-[200px] lg:h-[140px]">
-        <div className="main-container hidden lg:flex mx-auto gap-8">
-          <div className='cs-back-image'></div>
-
-          <div className="grid grid-cols-1 md:grid-cols-3  main-container max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] scLg:max-w-[700px] scLg:pl-10">
+        <div className="main-container hidden lg:flex mx-auto scLg:gap-7">
+          <div className='cs-back-image'>
+            <div className="w-[526px]"></div>
+          </div>
+          <div className="flex max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] items-center self-start">
+          <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-4 md:gap-6 xl:gap-[40px]">
             <div className="flex flex-col">
               <span className="font-gilroyBold text-custom-heading text-xl">
                 PROJECT TYPES
@@ -74,6 +76,7 @@ const SodexoLeveragesHero = () => {
                 Integrated Facilities Management
               </span>
             </div>
+          </div>
           </div>
         </div>
       </div>
