920c246383fae8f0cd02cafd6005ced109ab4d4b
Abhishek-Ideafueled <abhishek.kumar@ideafueled.com>
2024-09-24 12:32:55 +0530

    fixed sodexo drives grid alignment and image alignment

--- src/components/CaseStudiesInner/CsSodexoDrives/SodexoDrivesHero.jsx
+++ src/components/CaseStudiesInner/CsSodexoDrives/SodexoDrivesHero.jsx
@@ -12,15 +12,15 @@ const SodexoDrivesHero = () => {
       </div> */}
       <div className="w-full bg-custom-back h-[700px] smalls:h-[700px] md:h-[600px] lg:h-[450px] pt-10">
         <div className="flex flex-col-reverse lg:gap-8 lg:flex-row main-container w-full mx-auto">
-          <div className="relative cs-back-image">
+          <div className="relative sodexo-drives cs-back-image">
             <div className="lg:absolute scLg:static scLg:w-[526px] bg-cs-hero-img bg-center bg-cover min-w-[300px] lg:w-[1000px] h-[300px] md:min-h-[350px] lg:h-[500px] top-[0px] bottom-0 right-0 rounded-2xl bg-no-repeat"></div>
           </div>
 
           <div className="flex flex-col max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] items-center self-start py-5">
             <div className="flex flex-col lg:h-[344px] w-full">
-              <span className="h-[29px] lg:h-full">
+              <span className="h-[46px] lg:h-full">
                 <img
-                  className="object-cover h-[29px]"
+                  className="object-cover h-[46px]"
                   src="/sodexo-logo2.png"
                   alt="sodexo-logo"
                 />
@@ -54,10 +54,12 @@ const SodexoDrivesHero = () => {
         </div>
       </div>
       <div className="w-full bg-[#5BC0ED] py-10 h-[200px] sm:h-[150px] lg:h-[140px]">
-        <div className="main-container hidden lg:flex mx-auto gap-8">
-          <div className='cs-back-image'></div>
-
-          <div className="grid grid-cols-1 md:grid-cols-3  main-container max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] scLg:max-w-[700px] scLg:pl-10">
+        <div className="main-container hidden lg:flex mx-auto scLg:gap-8">
+          <div className='cs-back-image'>
+            <div className="w-[526px]"></div>
+          </div>
+        <div className='flex max-w-[600px] xl:max-w-[800px] xl:min-w-[700px] items-center self-start'>
+          <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-4 md:gap-6 xl:gap-[35px] scLg:gap-4">
             <div className="flex flex-col">
               <span className="font-gilroyBold text-custom-heading text-xl">
                 PROJECT TYPES
@@ -82,6 +84,7 @@ const SodexoDrivesHero = () => {
                 Integrated Facilities Management
               </span>
             </div>
+          </div>
           </div>
         </div>
       </div>
